636bdd95e94fb52922dee94a2df31052f9c95f25
GiselleMR <giselle.rubin1@gmail.com>
2023-04-12 10:19:55 -0400

    add google book api to app.js

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -7,9 +7,16 @@ import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
 
-export default function App() {
+
+export default async function App() {
   const [user, setUser] = useState(getUser());
 
+try{
+  const response = await
+fetch(`https://www.googleapis.com/books/v1/volumes?q=flowers&key=${process.env.REACT_APP_GoogleAPIPageTurner}`);
+const jsonData = await response.json();
+  console.log(jsonData);
+} catch(err){console.log(err)}
   return (
     <main className="App">
       { user ?
